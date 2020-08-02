47991e88c2b769647259171630e62045301ff6e2
tayta21 <ahmedtritar21@gmail.com>
2020-08-02 03:44:59 +0100

    Updates

---
+++ precache-manifest.261c1216300a90a9b1f326f4581ba384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf3bf7646908ed8127daa1d4fc46771",
+    "url": "/props/index.html"
+  },
+  {
+    "revision": "720ba56beaff3b4a5d12",
+    "url": "/props/static/css/main.51f0cadd.chunk.css"
+  },
+  {
+    "revision": "f90c33ab241120eae6c9",
+    "url": "/props/static/js/2.e348fba8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/props/static/js/2.e348fba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720ba56beaff3b4a5d12",
+    "url": "/props/static/js/main.997b655a.chunk.js"
+  },
+  {
+    "revision": "83c9e8c677715dfc30ab",
+    "url": "/props/static/js/runtime-main.08d9086c.js"
+  }
+]);
